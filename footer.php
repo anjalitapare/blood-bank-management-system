2deffaa04878cb168a130846bf1ab52c246cb52e
AnjaliTapare <136685972+anjalitapare@users.noreply.github.com>
2023-07-14 22:39:35 +0530

    add initial blood-bank- management system files

---
+++ footer.php
@@ -0,0 +1,29 @@
+<html>
+<head>
+  <style>
+  #footer {
+
+  position:absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 75px;
+  background-color:#000000;
+  color:white;
+  text-align: center;
+}
+  </style>
+</head>
+<body>
+  <div id="footer" >
+  <b><center>website devolps by Anjali Tapare <br>
+  Blood Bank & Donation Management
+  <br>
+CSE depatment from 3rd year section (B)2022
+  </center>
+  </div>
+
+
+</body>
+
+</html>
